394cde4cd7093c0530c1fa07924e8951f5d41377
Tony Villa <tonyvilla77@gmail.com>
2020-12-21 15:07:37 -0800

    Closure challenge

--- script.js
+++ script.js
@@ -333,6 +333,8 @@ runOnce();
 */
 
 //////////////////// CLOSURES
+
+/*
 // A closure is not a feature that we explcitely use. It simply happens automatically in certain situations, we just have to recognize those situations
 
 const secureBooking = function () {
@@ -377,3 +379,91 @@ booker();
 
 // We can, however see the closure with:
 console.dir(booker);
+*/
+
+///////////////////////// CLOSURE EXAMPLES
+
+/*
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f(); // At this point the g() variable environment is no longer on the stack. But the  (a) variable is inside f()'s backpack.
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000; // The closure is looking into it's original variable environment scope before looking into any other scope.
+boardPassengers(180, 3);
+*/
+
+///////////// Coding Challenge #2
+
+/*
+
+This is more of a thinking challenge than a coding challenge 🤓
+Your tasks:
+1. Take the IIFE below and at the end of the function, attach an event listener that
+changes the color of the selected h1 element ('header') to blue, each time
+the body element is clicked. Do not select the h1 element again!
+2. And now explain to yourself (or someone around you) why this worked! Take all
+the time you need. Think about when exactly the callback function is executed,
+and what that means for the variables involved in this example.
+
+*/
+let colorChange;
+
+(function () {
+  let header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  colorChange = function () {
+    header.style.color = 'blue';
+    console.log('click on body');
+  };
+})();
+
+const test = document
+  .querySelector('body')
+  .addEventListener('click', colorChange);
+
+console.dir(colorChange);
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
